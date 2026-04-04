123df12dc68893c277ba91eecbe54d569589c7b0
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-04-04 15:52:01 +0300

    v1

---
+++ KonsolUygulamaları/BasitHesaplar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasitHesaplar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1;
+            int s2;
+            int toplam;           
+
+            s1 = 35;
+            s2 = 45;
+
+            toplam = s1 + s2;
+
+            Console.WriteLine("Toplam:" + toplam);
+
+        }
+    }
+}
---
+++ KonsolUygulamaları/Değişkenler/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+
+namespace Değişkenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Değişken Ders Notları:
+             * Değişken, bazı verileri bilgisayarın belleğinde (RAM) geçici bir süre bekletmek için kullandığımız yapılardır. İçlerinde bilgiyi program boyunca saklarlar, program sonlandığında bellekten atılırlar. Değişkenler içlerinde değişik türde bilgiler taşıyabilirler. Bir değişkeni tanımlarken, içinde ne tür bir bilgi saklayacağımızı belirtmemiz gerekir.
+            Değişken tipleri:
+            1- Değer Tipli Değişkenler: byte, int, double, char, bool, struct, enum
+            2- Referans Tipli Değişkenler: string, array, class, delegate, interface
+
+            1- Değer Tipli Değişkenler: Bu tip değişkenleriçlerinde veri taşırlar. Belleğin "Stack" bölgesinde yaratılırlar.
+                a) Tam sayı tipler:  byte, short, int, long en çok kullanılan tam sayı tiplerdir.
+            
+             
+             
+             
+             
+             
+             */
+
+            //tam sayı tipler
+            byte notu = 35; // 1 byte
+            short notu2 = 200; // 2 byte
+            int maas = 2; // 4 byte
+            long büyükSayı = 3232132132131; // 8 byte
+
+            //ondalıklı sayı tipleri;
+            double ondalıklıSayı = 25000000;
+            double kdvOrani = 0.2;
+
+            notu2 = notu; // bilinçsiz (implicit) tip dönüşümü
+            notu = (byte)notu2; //type casting 1 (bilinçli - explicit) tip dönüşümü
+
+            //  maas = Convert.ToInt32("50000");
+
+
+
+            Console.WriteLine("---- Girilen 2 sayının toplamını veren program ------------");
+            
+            double sayi1;
+            double sayi2;
+            double toplam;
+
+            //double sayi1, sayi2, toplam; // alternatif
+
+            Console.Write("1. Sayıyı giriniz:");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("2. Sayıyı giriniz:");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            toplam = sayi1 + sayi2;
+
+            Console.WriteLine("Toplam " + toplam + " dır.");
+
+            Console.WriteLine("---- Girilen 1 sayının tek mi, çift mi olduğunu veren program ----");
+
+            int s1;
+            int sonuc;
+
+            Console.Write("1 sayı giriniz: ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+
+            sonuc = s1 % 2;
+
+            if (sonuc==1)
+            {
+                Console.WriteLine("Sayı Tekdir.");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Çifttir.");
+            }
+
+            Console.WriteLine("-- Girilen 2 notun ortalamasını hesaplayıp ekrana yazan program   --");
+
+            double not1, not2, ortalama;
+
+            Console.Write("Öğrencinin 1. sınav notunu giriniz:");
+            not1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Öğrencinin 2. sınav notunu giriniz:");
+            not2 = Convert.ToDouble(Console.ReadLine());
+
+            ortalama = (not1+not2)/2;
+
+            if (ortalama>=70)
+            {
+                Console.WriteLine("Geçtiniz. Ortalamanız: " + ortalama);
+            }
+            else
+            {
+                Console.WriteLine("Kaldınız. Ortalamanız: " + ortalama);
+            }
+
+            //char ve string --------------------------------
+            //-------------------------------------------------
+
+            Console.WriteLine("--- char ve string -----------------------------");
+
+            // char tek karakterlik bilgi tutan değişken tipidir.
+            char karakter = 'A';
+            char harf = (char)65;
+
+
+            //string ise birden fazla karakter tutabilen değişken tipidir.
+
+            string ögrencininAdıSoyadı = "Ali Muzaffer Kılıç";
+            string ürünMarkası = "Mercedes";
+
+            Console.WriteLine(ögrencininAdıSoyadı);
+            Console.WriteLine(harf);
+
+
+            Console.WriteLine("-- Girilen kullanıcı adı ve şifresini kontrol eden program ----");
+
+            string kullanıcı, şifre;
+
+            Console.Write("Kullanıcı adınızı giriniz:");
+            kullanıcı = Console.ReadLine();
+
+            Console.Write("Şifrenizi giriniz:");
+            şifre = Console.ReadLine();
+
+            if (kullanıcı == "vedat" && şifre == "12345")
+            {
+                Console.WriteLine(kullanıcı + " Hoşgeldin");
+            }
+            else 
+            {
+                Console.WriteLine("Kullanıcı adı ya da şifresi hatalı");     
+            }
+
+
+
+        }
+    }
+}
