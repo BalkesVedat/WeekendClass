107b35e3e4006f7bce174711068059cc1a333cec
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-04-11 15:54:40 +0300

    Add files via upload

---
+++ MyProjects/Input/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bu programda kullanıcıdan 2 tane sayı istenecek ve toplanıp sonucu ekrana yansıtılacak.
+
+            /* 
+             
+            Çok 
+            satırlı
+            not yazma alanı
+             
+             */
+
+            int sayi1 = 0;
+            int sayi2 = 0;
+            int toplam = 0;
+
+            try
+            {
+                Console.Write("1. Sayıyı giriniz:");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("2. sayıyı giriniz:");
+                sayi2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bir hata oluştu. Lütfen tekrar deneyin.");
+            }  
+
+            toplam = sayi1 + sayi2;
+            Console.WriteLine(toplam);
+
+            Console.WriteLine("Programı sonlandırmak için herhangi bir tuşa basınız...");
+
+           Console.ReadKey();
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    struct Student
+    {
+        public int StudentID;
+        public string NameSurname;
+        public double Grade;
+    }
+
+    struct Car
+    {
+        public int CarID;
+        public string Producer;
+        public string Model;
+        public int ProduceYear;
+        public Colors Color;
+        public double Price;
+    }
+
+    enum Colors
+    {
+        Black = 0,
+        Red = 5,
+        Yellow = 17,
+        Green = 4,
+        Gray = 6,
+        White = 1
+    }
+
+    enum Gender
+    {
+        Erkek,
+        Kadın
+    }
+
+
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            char karakter = 'a';
+            string k = "abcde";
+
+            k = "aaaaa";
+
+            char eleman = k[0];
+            char cr = (char)169;
+
+            Console.WriteLine(cr);
+
+            bool status = false;
+
+            Student s1;
+
+            s1.StudentID = 1;
+            s1.NameSurname = "Ali Ak";
+            s1.Grade = 2.88;
+
+
+
+            Car car1;
+
+            car1.CarID = 1;
+            car1.Producer = "Mercedes";
+            car1.Price = 11500000.50;
+            car1.Color = Colors.Black;
+           // car1.Color = (Colors)5;
+            car1.Model = "S600";
+            car1.ProduceYear = 2020;
+
+            
+
+            Console.WriteLine( s1.NameSurname );
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine( car1.Producer + " " + car1.Price);
+
+
+
+        }
+    }
+}
