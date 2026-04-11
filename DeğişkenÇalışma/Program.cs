b277da2f8d26753c461e53c24865087ba4398cc5
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-04-11 15:53:54 +0300

    Add files via upload

---
+++ DeğişkenÇalışma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeğişkenÇalışma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.Write("Ürün Adedi Giriniz:");
+            //double urunAdedi = Convert.ToDouble(Console.ReadLine());
+
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.Write("Ürün Fiyatını giriniz:");
+            //double urunFiyatı = Convert.ToDouble(Console.ReadLine());
+
+            //double Tutar = urunAdedi * urunFiyatı;
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine("Tutar:" + Tutar);
+
+            //Console.ResetColor();
+
+            //----------------------------------------------------
+
+            //double ort = vize * 0.4 + final * 0.6 ;
+
+          
+
+
+
+
+        }
+    }
+}
