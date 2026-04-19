7696710a8ad0e3b987057b15a6012e02236d5184
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-04-19 20:01:16 +0300

    Add files via upload

---
+++ Döngüler/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Döngüler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Döngü : Birden fazla kere çalışması gereken kodları tekrar tekrar yazmak yerine, bu kodu istediğimiz kadar çalıştırmayı sağlayan yapılara döngü denir. Bir döngü ya belli bir adette ya da belli bir şartın sağlanması durumunda çalışacak şekilde programlanabilir.
+
+            // Yanlış yöntem
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+
+            //*********** 1 CUSTOM ITERATION *****************************
+            int sayac = 0;
+        tekrar:
+            if (sayac % 3 == 0)
+                Console.WriteLine(sayac);
+           
+            sayac++;
+            if (sayac < 100)
+                goto tekrar;
+            //************************************************************
+
+            // ***************  FOR  DÖNGÜSÜ *****************************
+            // İşlemin belli bir adette yapılması istenildiğinde tercih edilen yöntemdir.
+
+            Console.WriteLine("------------ for ------------------------");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("----------------------------");
+            Console.Write("İsminizi Girin :");
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]) ;          
+            }
+
+            Console.WriteLine("----------------------------------------------");
+
+            string[] iller = {"İstanbul","Ankara","İzmir","Adana","Mersin","Hakkari","Trabzon"};
+
+            for (int i = 0; i < iller.Length; i++)
+            {
+                Console.WriteLine(iller[i]);
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            int j = 0;
+
+            for ( ; ; ) // FOR parantezine yazılan ifadeler zorunlu değildir. Farklı şekilde tanımlanabilir.
+            {
+                if (j < iller.Length)
+                {
+                    Console.WriteLine(iller[j]);
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("------------- foreach ----------------");
+
+            //foreach : Bir koleksiyondaki elemanların her biri için ayrı ayrı tek tek çalışması gereken bir işlem olduğunda tercih ettiğimiz döngü yapısıdır.
+
+            foreach (var item in iller)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("--------- sözlük ----------------");
+
+            Dictionary<string, string> enTrsozluk = new Dictionary<string, string>();
+
+            enTrsozluk.Add("mouse", "fare");
+            enTrsozluk.Add("pen", "tükenmez kalem");
+            enTrsozluk.Add("cup", "bardak");
+            enTrsozluk.Add("car", "araba");
+            enTrsozluk.Add("phone", "telefon");
+            enTrsozluk.Add("class", "sınıf");
+            enTrsozluk.Add("dictionary", "sözlük");
+
+            foreach (string item in enTrsozluk.Keys)
+            {
+                Console.WriteLine($"{item} : {enTrsozluk[item]}");
+            }
+
+            Console.WriteLine("------------ while ------------------");
+
+            // WHILE döngüsü : Eğer döngünün çalışması ya da durması bir koşula bağlı ise WHILE yapısını tercih ederiz.
+
+            string cevap ="E";
+
+            while (cevap == "E")
+            {
+                //TODO:Hesap makinası buraya monte edilecek.
+
+                Console.Write("Devam Etmek istiyor musunuz? (E/H)");
+                cevap = Console.ReadLine().ToUpper();
+            }
+
+            int adet = 0;
+
+            while (adet < iller.Length)
+            {
+                Console.WriteLine(iller[adet]);
+                adet++;
+            }
+
+            Console.WriteLine("----------- do while -----------");
+
+            //do
+            //{
+
+            //} while (koşul);
+
+            string cevap2="";
+
+            do
+            {
+                //TODO:Hesap makinası buraya monte edilecek.
+
+                Console.Write("Devam Etmek istiyor musunuz? (E/H) ");
+                cevap2 = Console.ReadLine().ToUpper();
+            } while (cevap2 == "E");
+
+
+        }
+    }
+}
--- Operatorler/Program.cs
+++ Operatorler/Program.cs
@@ -11,17 +11,63 @@ namespace Operatorler
         static void Main(string[] args)
         {
             //Operatorler : 
-            //1.Aritmetik Operatörler : +, -, *, /, % (mod)
+            //1.Aritmetik Operatörler : +, -, *, /, % (mod) , ( , )
             /*
             Sayıların arasına konan ve matematiksel işlem yapılmasını sağlayan operatörlerdir.  
                          */
-            //2. Mantıksal karşılaştırma Operatörleri: <, >, <=, >=, ==, !=, ===
+
+            //double kdvTutarı = 175 + 0.2 * 175;
+
+            //int urun1adet = 5;
+            //int urun2adet = 27;
+
+            //int fiyat = 500;
+
+            //double tutar = (urun1adet + urun2adet) * fiyat;
+
+            //2. Mantıksal karşılaştırma Operatörleri: <, >, <=, >=, ==, !=
 
             // bool sonuc = x != y ;  bool kontrol = x == 0;
-            
-            // TODO: === kontrol edilecek. 
+            // karşılaştırma sonucu olumlu ise geriye true değilse false dönerler.
 
-            Console.WriteLine();
+            //3. Atama Operatörü:  =  
+            //sağındaki bilgiyi, sol tarafa aktarır.   örn: x = 5; sonuc = a+b;
+
+            //4. Birleşik operatörler :  ++, --, += , -=, *= , /=
+            // Bazı işlemlerin kısa tanımı için kullanılırlar.
+
+            // örnek:
+            int sayac = 0;
+
+            sayac = sayac + 1;
+
+            sayac += 1; // 1 arttırır.
+
+            sayac += 3; // eşittir in sağındaki değer kadar arttırır.
+
+            sayac++; // 1 arttıran operatör.
+
+            sayac--; // 1 eksiltir.
+
+            sayac -= 2; // eşittir in sağındaki değer kadar eksiltir.
+
+            sayac *= 2;
+
+            sayac /= 3;
+
+            //5. metin birleştirme operatörü:  +  
+            //  +  işaretinin sağındaki ya da solundaki veri string ise o zaman metin birleştirme işlemi yapar ve string değer üretir. her iki değer sayısal ise aritmetik toplama yapar ve toplama sonucunu geriye uygun tipte döndür. 
+
+            string kod = "ABC" + 123;
+
+            Random rnd = new Random();
+
+            string password = "P" + rnd.Next(1000000, 9999999) + "!";
+
+            string ss = "ali" + (5 + 3);
+
+
+            Console.WriteLine(ss);
 
 
         }
